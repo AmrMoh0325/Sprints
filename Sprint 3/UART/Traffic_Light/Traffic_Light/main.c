b2ce831827a8bafae8b67ed91ef66cf280d816e6
Amr Mohamed <Amrmoh0325@gmail.com>
2021-07-17 12:05:39 +0200

    done with comments

--- Sprint 3/UART/Traffic_Light/Traffic_Light/main.c
+++ Sprint 3/UART/Traffic_Light/Traffic_Light/main.c
@@ -6,15 +6,18 @@
 * Date: 15/7/2021
 ******************************************************************************/ 
 
+/* INCLUDES ------------------------------------------------------------*/
 #include "APP/Traffic_Light/Traffic.h"
 
 
-
+/* API IMPLEMENTATION -------------------------------------------------- */
 int main(void)
 {
+   //call initialization function
    Traffic_Init();
    while (1) 
    {
+      //update the state of the system depending on user input
       Traffic_Update();
    }
 }
