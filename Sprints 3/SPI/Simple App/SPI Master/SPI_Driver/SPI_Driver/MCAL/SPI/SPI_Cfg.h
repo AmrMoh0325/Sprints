acfd36b00d84a777607612d19ae8328c3f258ab7
Amr Mohamed <Amrmoh0325@gmail.com>
2021-07-18 13:42:32 +0200

    restore project

---
+++ Sprints 3/SPI/Simple App/SPI Master/SPI_Driver/SPI_Driver/MCAL/SPI/SPI_Cfg.h
@@ -0,0 +1,20 @@
+/*
+ * SPI_Cfg.h
+ *
+ * Created: 7/15/2021 8:19:45 PM
+ *  Author: Amrth
+ */ 
+
+
+#ifndef __SPI_CFG__
+#define __SPI_CFG__
+
+
+#define MASTER    1
+#define SLAVE     0
+
+#define SPI_CONFIG      MASTER
+
+
+
+#endif /* __SPI_CFG__ */
