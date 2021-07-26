1d4df217b1a5d138f5d80631d772a34455442209
Amr Mohamed <Amrmoh0325@gmail.com>
2021-07-26 19:57:20 +0200

    done with app, comments and flowchart

--- Sprint 3/Thermostat/Project/Thermostat/Thermostat/MCAL/DIO/DIO.h
+++ Sprint 3/Thermostat/Project/Thermostat/Thermostat/MCAL/DIO/DIO.h
@@ -1,5 +1,5 @@
 /*****************************************************************************
-* Task: AVR_DRIVERS
+* Task: DIO Driver
 * File Name: DIO.h
 * Description: File for DIO.c prototypes
 * Author: Amr Mohamed
@@ -9,18 +9,36 @@
 #ifndef __DIO__
 #define __DIO__
 
-
+ /*- INCLUDES --------------------------------------------------*/
 #include "DataTypes.h"
 #include "Utils.h"
+#include "MCAL/DIO/DIO_Cfg.h"
 
-#define DIO_MC_PINS    ALL_PINS
-
-typedef enum {OUTPUT,INPLUP,INFREE} enuDIOPinType_t;
+/*- ENUMS --------------------------------------------------*/
+typedef enum {INPUT, OUTPUT} enuDIOPinDir_t;
+typedef enum {NO_CONNECTION, PULL_UP_ENABLE} enuDIOPullupResEn_t;
 typedef enum {PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7,
               PB0, PB1, PB2, PB3, PB4, PB5, PB6, PB7,
               PC0, PC1, PC2, PC3, PC4, PC5, PC6, PC7,
-              PD0, PD1, PD2, PD3, PD4, PD5, PD6, PD7,ALL_PINS} enuDIOPinNo_t;
+              PD0, PD1, PD2, PD3, PD4, PD5, PD6, PD7} enuDIOPinNo_t;
 
+
+/*- STRUCTS --------------------------------------------------*/
+typedef struct
+{
+   /* Selecting the pin number in that specific port selected */
+   enuDIOPinNo_t enuPinNo;
+   /* Selecting the direction of data on the selected pin */
+   enuDIOPinDir_t enuPinDir;
+   /* Selecting whether a pull-up resistor is connected to the selected pin or not */
+   enuDIOPullupResEn_t enuPullupResEn;
+} strDIOConfigType_t;
+
+/*- GLOBAL VARIABLES --------------------------------------------------*/
+extern const strDIOConfigType_t astrDIOConfigParameters[DIO_GROUPS_NO];
+
+
+/*- APIs PROTOTYPES-----------------------------------*/
 /************************************************************************************
 * Parameters (in): void
 * Parameters (out): enuErrorStatus_t
@@ -30,27 +48,27 @@ typedef enum {PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7,
 enuErrorStatus_t DIO_Init(void);
 
 /************************************************************************************
-* Parameters (in): enuDIOPinNo_t PinId, uint8_t u8Data
+* Parameters (in): uint8_t u8GroupId, uint8_t u8Data
 * Parameters (out): enuErrorStatus_t
 * Return value: 1=SUCCESS or 0=FAIL
 * Description: A function to write a digital value into the selected DIO u8pin
 ************************************************************************************/
-enuErrorStatus_t DIO_Write(enuDIOPinNo_t PinId, uint8_t u8Data);
+enuErrorStatus_t DIO_Write(uint8_t u8GroupId, uint8_t u8Data);
 
 /************************************************************************************
-* Parameters (in): enuDIOPinNo_t PinId, uint8_t* pu8Data
+* Parameters (in): uint8_t u8GroupId, uint8_t* pu8Data
 * Parameters (out): enuErrorStatus_t
 * Return value: 1=SUCCESS or 0=FAIL
 * Description: A function to read a digital value from the selected DIO u8pin
 ************************************************************************************/
-enuErrorStatus_t DIO_Read(enuDIOPinNo_t PinId, uint8_t* pu8Data);
+enuErrorStatus_t DIO_Read(uint8_t u8GroupId, uint8_t* pu8Data);
 
 /************************************************************************************
-* Parameters (in): enuDIOPinNo_t PinId, uint8_t* pu8Data
+* Parameters (in): uint8_t u8GroupId, uint8_t* pu8Data
 * Parameters (out): enuErrorStatus_t
 * Return value: 1=SUCCESS or 0=FAIL
 * Description: A function to toggle the digital value of the selected DIO pin
 ************************************************************************************/
-enuErrorStatus_t DIO_Toggle(enuDIOPinNo_t PinId);
+enuErrorStatus_t DIO_Toggle(uint8_t u8GroupId);
 
 #endif /* __DIO__ */
---
+++ Sprint 3/Thermostat/Project/Thermostat/Thermostat/main.c
@@ -0,0 +1,18 @@
+/*
+ * Thermostat.c
+ *
+ * Created: 7/26/2021 7:50:47 PM
+ * Author : Amrth
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
