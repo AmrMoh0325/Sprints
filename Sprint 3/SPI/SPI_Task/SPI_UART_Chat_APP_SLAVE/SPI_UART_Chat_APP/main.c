09b5a261870d5b079dc683e82e229e9f75b252c8
Amr Mohamed <Amrmoh0325@gmail.com>
2021-07-27 20:35:37 +0200

    tested

--- Sprint 3/SPI/SPI_Task/SPI_UART_Chat_APP_SLAVE/SPI_UART_Chat_APP/main.c
+++ Sprint 3/SPI/SPI_Task/SPI_UART_Chat_APP_SLAVE/SPI_UART_Chat_APP/main.c
@@ -1,12 +1,15 @@
-/*
- * SPI_UART_Chat_APP.c
- *
- * Created: 7/16/2021 8:20:07 PM
- * Author : Amrth
- */ 
+/*****************************************************************************
+* Task: Chat Application
+* File Name: main.c
+* Description: main file
+* Author: Amr Mohamed
+* Date: 16/7/2021
+******************************************************************************/
 
+/*- INCLUDES --------------------------------------------------*/
 #include "APP/Chat_APP/APP.h"
 
+/*- APIs IMPLEMENTATION-----------------------------------*/
 int main(void)
 {
    APP_Init_Slave();
